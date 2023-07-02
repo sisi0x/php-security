28a66bd8d007a9f27245a8d76b93de866eb0642b
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-02 04:46:22 +0300

    Update sqli.php

--- sqli.php
+++ sqli.php
@@ -8,22 +8,27 @@
  
 
 <?php
-    ini_set('display_errors','on');
 
-include 'connect_sqli.php';
+//##################### 
+// old code 
+//#################
+
+//     ini_set('display_errors','on');
+
+// include 'connect_sqli.php';
 // if(isset($_GET['id']) && ! empty($_GET['id'])) {
 //  $userid= $_GET['id'];
 //  prepare 
- $stmt = $con->prepare("SELECT * FROM user WHERE id =1");
+//  $stmt = $con->prepare("SELECT * FROM user WHERE id =1");
 
- $stmt->execute();
+//  $stmt->execute();
 
- $count= $stmt->rowCount();
- while($row = $stmt->fetch()){
-  $id = $row['id'];
-  $name = $row['name'];
- }
-echo $name;
+//  $count= $stmt->rowCount();
+//  while($row = $stmt->fetch()){
+//   $id = $row['id'];
+//   $name = $row['name'];
+//  }
+// echo $name;
 // if($count > 0){
 //   echo $name;
 // }else{
@@ -34,13 +39,17 @@ echo $name;
 // }
 
 
+// ##################### 
+// New code 
+// #################
 
-
-//code not faxt
+// ################# 
+//     code not fixt 
+// #######################
 
 // $servername = "localhost";
-// $username = "m5awy";
-// $password = "password";
+// $username = "sisidb";
+// $password = "123456";
 // $dbname = "test";
 // if (isset($_GET['id'])){
 //     $id = $_GET['id'];
@@ -60,79 +69,70 @@ echo $name;
 // }
 // $conn->close();
 // }
-//code fixt
 
-/**
- * Check if the 'id' GET variable is set
- * Example - http://localhost/?id=1
- */
-// if (isset($_GET['id'])){
-//   $id = $_GET['id'];
-//   /**
-//    * Validate data before it enters the database. In this case, we need to check that
-//    * the value of the 'id' GET parameter is numeric
-//    */
-//    if ( is_numeric($id) == true){
-//     try{ // Check connection before executing the SQL query 
-//       /**
-//        * Setup the connection to the database This is usually called a database handle (dbh)
-//        */
-//       $dbh = new PDO('mysql:host=localhost;dbname=test', 'm5awy', 'password');
-//       /**
-//        * Use PDO::ERRMODE_EXCEPTION, to capture errors and write them to
-//        * a log file for later inspection instead of printing them to the screen.
-//        */
-//       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//       /**
-//        * Before executing, prepare statements by binding parameters.
-//        * Bind validated user input (in this case, the value of $id) to the
-//        * SQL statement before sending it to the database server.
-//        *
-//        * This fixes the SQL injection vulnerability.
-//        */
-//       $q = "SELECT * FROM sqli WHERE ID = :id";
-//       // Prepare the SQL query string.
-//       $sth = $dbh->prepare($q);
-//       // Bind parameters to statement variables.
-//       $sth->bindParam(':id', $id);
-//       // Execute statement.
-//       $sth->execute();
-//       // Set fetch mode to FETCH_ASSOC to return an array indexed by column name.
-//       $sth->setFetchMode(PDO::FETCH_ASSOC);
+
+// ###################### 
+//    code fixt
+// ##########################
+
+// 1 Check if the 'id' GET variable is set  Ex - http://localhost/?id=1
+ 
+if (isset($_GET['id'])){
+  $id = $_GET['id'];
+
+// 2-  Validate data before it enters the database. In this case, we need to check that
+// 3-  the value of the 'id' GET parameter is numeric
+
+   if ( is_numeric($id) == true){
+    try{ 
+//  4-  Check connection before executing the SQL query 
+//  5-  Setup the connection to the database This is usually called a database handle (dbh)
+
+      $dbh = new PDO('mysql:host=localhost;dbname=test', 'sisidb', '123456');
+
+//    Use PDO::ERRMODE_EXCEPTION, to capture errors and write them to
+//    a log file for later inspection instead of printing them to the screen.
+
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//   6- Before executing, prepare statements by binding parameters.
+//   7- Bind validated user input (in this case, the value of $id) to the
+//   8- SQL statement before sending it to the database server.
+
+//         This fixes the SQL injection vulnerability.
+
+      $q = "SELECT * FROM sqli WHERE ID = :id";
+       // Prepare the SQL query string.
+       $sth = $dbh->prepare($q);
+      // Bind parameters to statement variables.
+      $sth->bindParam(':id', $id);
+       // Execute statement.
+      $sth->execute();
+      // Set fetch mode to FETCH_ASSOC to return an array indexed by column name.
+      $sth->setFetchMode(PDO::FETCH_ASSOC);
 //       // Fetch result.
-//       $result = $sth->fetchColumn();
-//       /**
-//        * HTML encode our result using htmlentities() to prevent stored XSS and print the
-//        * result to the page
-//        */
-//       print( "ID: " . htmlentities($result) );
-//       //Close the connection to the database.
-//       $dbh = null;
-//     }
-//     catch(PDOException $e){
-//       /**
-//        * You can log PDO exceptions to PHP's system logger, using the
-//        * log engine of the operating system
-//        *
-//        * For more logging options visit http://php.net/manual/en/function.error-log.php
-//        */
-//       error_log('PDOException - ' . $e->getMessage(), 0);
-//       /**
-//        * Stop executing, return an Internal Server Error HTTP status code (500),
-//        * and display an error
-//        */
-//       http_response_code(500);
-//       die('Error establishing connection with database');
-//     }
-//    } else{
-//     /**
-//      * If the value of the 'id' GET parameter is not numeric, stop executing, return
-//      * a 'Bad request' HTTP status code (400), and display an error
-//      */
-//     http_response_code(400);
-//     die('Error processing bad or malformed request');
-//    }
-// }
+      $result = $sth->fetchColumn();
+    //   Close the connection to the database.
+      $dbh = null;
+    }
+    catch(PDOException $e){
+
+      error_log('PDOException - ' . $e->getMessage(), 0);
+
+//   9- Stop executing, return an Internal Server Error HTTP status code (500),
+//       and display an error
+
+      http_response_code(500);
+      die('Error establishing connection with database');
+    }
+   } else{
+
+//      If the value of the 'id' GET parameter is not numeric, stop executing, return
+//      a 'Bad request' HTTP status code (400), and display an error
+     http_response_code(400);
+    die('Error processing bad or malformed request');
+    }
+ }
 ?>
  
  </body>
