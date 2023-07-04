a5f57baae55cf413a563e97688898cfa61e5ced8
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-05 01:35:23 +0300

    Update index.php

--- CSRF/index.php
+++ CSRF/index.php
@@ -11,7 +11,7 @@
    
     <input type="submit" value="delete my account">
     <input type="hidden" name="_token" value="<?php $_SESSION['_token'];?>"/>
-<!--Too see the token and After you see the token, make a comment (//) -->   
+<!--Here you will see The token  then you should put a comment (//) -->   
     <?php echo $_SESSION['_token']=bin2hex(random_bytes(32)); ?>
   </form>
 </body>
