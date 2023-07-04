d51ce308c7e7c53596c975b38082f055de128196
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-05 01:33:05 +0300

    Update delete.php

--- CSRF/delete.php
+++ CSRF/delete.php
@@ -2,8 +2,13 @@
 // ####################
 // code not fix
 //######################
-// require 'connect.php';
 
+// require 'connect.php';
+/*
+
+ No in check for Token or METHOD 
+
+*/
 // $query = $db->prepare("UPDATE users SET status = 0 WHERE id = :user_id");
 // $query->execute([
 //   'user_id' => $_SESSION['user_id'],
@@ -18,11 +23,25 @@
 
 require 'connect.php';
 
+
+ /*
+ Here to check Method only Post not Get becoes on labe portsweger you can pybass
+ The check 
+ you can see the labe from here
+*/
+
+
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
 
   die('Invalid operation');
 
 }
+
+/*
+
+
+
+*/
 if(!isset($_POST['_token']) || ($_POST['_token'] !== $_SESSION['_token'])){
         
   die("Invalid token");
