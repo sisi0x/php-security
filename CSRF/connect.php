a0054e9f83f86016929021b14beb891f46a61d92
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-05 03:46:15 +0300

    Update connect.php

--- CSRF/connect.php
+++ CSRF/connect.php
@@ -1,73 +1,37 @@
-<?php
+<?php 
 // ####################
 // code not fix
 //######################
+// session_start();
+// $_SESSION['user_id'] = 1 ;
 
-/*
+// $db =  new PDO('mysql:host=127.0.0.1;dbname=CSRF','root','');
+?>
 
-# Here no check for anythink ,
-# Here you can see the lab for Example  (
-https://portswigger.net/web-security/csrf/lab-no-defenses 
-)
-
-*/
-
-// require 'connect.php';
-// $query = $db->prepare("UPDATE users SET status = 0 WHERE id = :user_id");
-// $query->execute([
-//   'user_id' => $_SESSION['user_id'],
-// ]);
-// ?>
 
 <?php 
-
-####################
+// ####################
 // code fix
-######################
+//######################
+session_start();
 
-/*
-
-To fix The vulnerable we nade :  
-1- create token and check the parameter send be REQUEST METHOD
-2- must The token  be strong , and the length must be no less than 16 And must gev the User new token in every requst
-2- we nade check The REQUEST_METHOD only POST 
+$_SESSION['user_id'] = 1 ;
 
 
-require 'connect.php';
-
-/*
-
-# Here you will see The token ,then you should remove (echo)   
-   
-*/
- 
-//  echo $_SESSION['_token']=bin2hex(random_bytes(32)); 
+$db =  new PDO('mysql:host=127.0.0.1;dbname=CSRF','root','');
 
 
-/*
-# Here The developer check The token ,but forget check the REQUEST METHOD
-# Here you can see the lab for  Example ( https://portswigger.net/web-security/csrf/bypassing-token-validation/lab-token-validation-depends-on-request-method )
-*/
-
-if($_SERVER['REQUEST_METHOD'] !== 'POST'){
-
-  die('Invalid METHOD');
-
-}
-
-
- /*
-  Here The developer check The token with SESSION
- */
-
- if(!isset($_POST['token']) || ($_POST['token'] !== $_SESSION['token'])){
+// // Chack if the METHOd POST not Get
+// if($_SERVER["REQUSET_METHOD"] === "POST"){
+//     //Chack the token 
+//     if(!isset($_POST["token"]) || ($_POST["token"] !== $_SESSION["token"])){
         
-  die("Invalid token");
- die("Missing parameter token");
+//         die("Invalid token");
+        
+//     }
+// }
 
- }
-$query = $db->prepare("UPDATE users SET status = 0 WHERE id = :user_id");
-$query->execute([
-  'user_id' => $_SESSION['user_id'],
-]);
- ?>
+
+$_SESSION['token']=bin2hex(random_bytes(32));
+
+?>
