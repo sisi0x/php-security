c966136255e2d2c72043761dbb5041c4ac7b894b
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-05 04:13:33 +0300

    Update connect.php

--- CSRF/connect.php
+++ CSRF/connect.php
@@ -1,37 +1,8 @@
 <?php 
-// ####################
-// code not fix
-//######################
-// session_start();
-// $_SESSION['user_id'] = 1 ;
 
-// $db =  new PDO('mysql:host=127.0.0.1;dbname=CSRF','root','');
-?>
-
-
-<?php 
-// ####################
-// code fix
-//######################
 session_start();
-
 $_SESSION['user_id'] = 1 ;
 
-
 $db =  new PDO('mysql:host=127.0.0.1;dbname=CSRF','root','');
-
-
-// // Chack if the METHOd POST not Get
-// if($_SERVER["REQUSET_METHOD"] === "POST"){
-//     //Chack the token 
-//     if(!isset($_POST["token"]) || ($_POST["token"] !== $_SESSION["token"])){
-        
-//         die("Invalid token");
-        
-//     }
-// }
-
-
-$_SESSION['token']=bin2hex(random_bytes(32));
 
 ?>
