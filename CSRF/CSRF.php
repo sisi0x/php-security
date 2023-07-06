4546f34ff282bd7c6248385e16eaeb40b038896d
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-07-07 00:28:45 +0300

    Update CSRF.php

--- CSRF/CSRF.php
+++ CSRF/CSRF.php
@@ -49,11 +49,9 @@ To fix The vulnerable we nade :
 */
           if (isset($_POST["token"]) == "token") 
           {
-
               echo "<br>";
               echo "vailed premeter .... (: ";
               
-
 /*
 
     # Check the token length
@@ -61,24 +59,25 @@ To fix The vulnerable we nade :
     # If the token length is valid,update the user status and display a success message 
     # Otherwise, display an error message and exit
 
-    */
-              if(isset($_POST["token"])) {
-                  $token_length = strlen($_POST["token"]);
-                  echo "<br>";  
-                  echo $token_length;
-                  if ($token_length == 64 ) { 
+    */  
+            if (strlen($_POST['token']) == 65) 
+            {
+              
+              echo "<br>"; 
+                echo strlen($_POST['token']);
+
                     echo "<br>";
                     echo "valid token length";
                     echo "<br>";
                     echo "Account deleted successfully";
                     $query = $db->prepare("UPDATE users SET status = 0 WHERE id = :user_id");
                     $query->execute(['user_id' => $_SESSION['user_id']]);
-                } else {
+            } else {
                   echo "<br>"; 
                   // herer you shoud use the die becaes if you ues echo you gest have hem  message error , but the attack successfully
                  die("Error: Invalid token length");
                 }
-              }
+              
           }else{
                 echo "<br>";
                 die("Invalied Premeter...!");
