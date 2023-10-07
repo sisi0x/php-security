acd5f3a9d74cf540ded0bb75963080fd640cc095
Omar Nasser <100969542+sisi0x@users.noreply.github.com>
2023-10-07 12:16:18 +0300

    Update file-upload.php

--- file-upload.php
+++ file-upload.php
@@ -15,54 +15,58 @@ $imageType = $_FILES['image']['type'];
 
 // Fiex code
 if (isset($_FILES['image']) == 'POST'){
-            // print_r($_FILES['image'])."<br>";
-            $file_name=strtolower($_FILES['image']['name']);
-            $file_size=$_FILES['image']['size'];
-            $file_tmp=$_FILES['image']['tmp_name'];
-            $file_type=$_FILES['image']['type'];
-            
-            echo "name is: " .$file_name."<br>";
-            echo "Size is: " .$file_size."<br>";
-            echo "Tmp is: " .$file_tmp."<br>";
-            echo "type is: " .$file_type."<br>";
-            
+    // print_r($_FILES['image'])."<br>";
+    $file_name=strtolower($_FILES['image']['name']);
+    $file_size=$_FILES['image']['size'];
+    $file_tmp=$_FILES['image']['tmp_name'];
+    $file_type=$_FILES['image']['type'];
+    
+    echo "name is: " .$file_name."<br>";
+    echo "Size is: " .$file_size."<br>";
+    echo "Tmp is: " .$file_tmp."<br>";
+    echo "type is: " .$file_type."<br>";
+    
 
-            $avalible_ext= array('jpeg','jpg','png') ;
-            $ext = end(explode('.',$file_name));
-            $imgage = rand(0,100000). '.' . $ext;
-            
-            //check for jprg,jp,png,image 
-            if(!in_array($ext,$avalible_ext))
-                {
-                die($error="invalid extension");
-                }else
-            $contntess = end(explode('/',$file_type));
-            echo "contntess ".  $contntess ."<br>";
-    //check content 
-        if($ext === $contntess){
-            $test = $ext.$contntess ;
-                if($file_type === 'image/jpeg' ||$file_type === 'image/jpg'||$file_type === 'image/png' ) 
-                {
+    $avalible_ext= array('jpeg','jpg','png') ;
+    $ext = end(explode('.',$file_name));
+    $imgage = rand(0,1337). '.' . $ext;
+    
+    
+    if ( $imgage[0] !== 1337 ||  $imgage[1] !== 1337) {
+        die("lol noob, your pic is not l33t enough");
+    }
+    //check for jprg,jp,png,image 
+    if(!in_array($ext,$avalible_ext))
+        {
+        die($error="invalid extension");
+        }else
+    $contntess = end(explode('/',$file_type));
+    echo "contntess ".  $contntess ."<br>";
+//check content 
+if($ext === $contntess){
+    $test = $ext.$contntess ;
+        if($file_type === 'image/jpeg' ||$file_type === 'image/jpg'||$file_type === 'image/png' ) 
+        {
+                
+            $inaled = ['%','%00'];
+           
+           // check for Null
+                    if(in_array($test,$inaled)){
+                        die("nop");}
+//check for metadata, But i don't know how can do this if know Please  Tell me ??
+//linkedin // https://www.linkedin.com/in/sisi0x/
                         
-                    $inaled = ['%','%00'];
-                   
-                   // check for Null
-                            if(in_array($test,$inaled)){
-                                die("nop");}
- //check for metadata, But i don't know how can do this if know Please  Tell me ??
- //linkedin // https://www.linkedin.com/in/sisi0x/
-                                
-                                if(!$error){
-                                    move_uploaded_file($file_tmp,$file_name);
-                                    echo "secces";}else {
-                                        echo "<pre>";
-                                        print_r($error."<br>");
-                                        echo "</pre>";}
-                                    
-               }else{echo "invaled content ".$file_type."<br>";}
-         // check for Content-Type
+                        if(!$error){
+                            move_uploaded_file($file_tmp,$file_name);
+                            echo "secces";}else {
+                                echo "<pre>";
+                                print_r($error."<br>");
+                                echo "</pre>";}
+                            
+       }else{echo "invaled content ".$file_type."<br>";}
+ // check for Content-Type
 
-        }else{die($error="invaled content");}
+}else{die($error="invaled content");}
 }else{echo "invalid Request"; };
 ?>
 <!DOCTYPE html>
